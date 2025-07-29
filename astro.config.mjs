d3b9ba54393fb88a8dec342d6e066a71c4d8d661
za-ibrahim <ibrahim@zeptoapps.com>
2025-07-29 19:32:40 +0600

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
@@ -15,7 +13,5 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()],
-  },
-
-  integrations: [react()],
+  }
 });
