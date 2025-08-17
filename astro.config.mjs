c72100c04e282e3de58c6a6e994f852e8317608f
za-ibrahim <ibrahim@zeptoapps.com>
2025-08-17 13:02:22 +0600

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,13 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    fonts: [{
-      provider: fontProviders.google(),
-      name: "Poppins",
-      cssVariable: "--font-poppins"
-    }]
-  },
-
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()]
 });
